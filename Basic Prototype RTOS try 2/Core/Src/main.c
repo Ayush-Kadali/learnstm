62907c5afac9803b245da38d9df2f2a89057b9e0
Ayush-Kadali <ayushkadali@gmail.com>
2025-01-21 21:26:29 +0530

    Added a basic prototype that works

--- Basic Prototype RTOS try 2/Core/Src/main.c
+++ Basic Prototype RTOS try 2/Core/Src/main.c
@@ -1,20 +1,4 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,74 +6,46 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+#include "string.h"
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define TEAM_ID "ASI-050"
-#define TELEMETRY_DELAY 1000
-#define ERROR_CHECK_DELAY 100
-#define MAX_TELEMETRY_STRING 256
-#define EEPROM_STATE_ADDR 0x0800F000
-
-
-typedef uint16_t ErrorFlags;
-#define E_NONE      0x0000
-#define E_BATT      0x0001
-#define E_COMM_LOS  0x0002
-#define E_COMM_DEL  0x0004
-#define E_MEM       0x0008
-#define E_SD        0x0010
-#define E_FLASH     0x0020
-#define E_OVER_MB   0x0040
-#define E_OVER_AB   0x0080
-#define E_TRAJECTORY 0x0100
-#define E_PARA      0x0200
-#define E_PAYLOAD   0x0400
-
+#define ERROR_NONE 0x0000
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
+int Recovered = 0;
 
-/* Definitions for stateTask */
-osThreadId_t stateTaskHandle;
-const osThreadAttr_t stateTask_attributes = {
-  .name = "stateTask",
+/* Definitions for idleTask */
+osThreadId_t idleTaskHandle;
+const osThreadAttr_t idleTask_attributes = {
+  .name = "idleTask",
   .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityHigh,
+  .priority = (osPriority_t) osPriorityNormal,
 };
 /* Definitions for telemetryTask */
 osThreadId_t telemetryTaskHandle;
 const osThreadAttr_t telemetryTask_attributes = {
   .name = "telemetryTask",
   .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityAboveNormal,
+  .priority = (osPriority_t) osPriorityRealtime,
 };
-/* Definitions for errorTask */
-osThreadId_t errorTaskHandle;
-const osThreadAttr_t errorTask_attributes = {
-  .name = "errorTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
-};
-/* Definitions for storageTask */
-osThreadId_t storageTaskHandle;
-const osThreadAttr_t storageTask_attributes = {
-  .name = "storageTask",
+/* Definitions for sdCardSimTask */
+osThreadId_t sdCardSimTaskHandle;
+const osThreadAttr_t sdCardSimTask_attributes = {
+  .name = "sdCardSimTask",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityLow,
 };
@@ -104,98 +60,56 @@ const osMutexAttr_t telemetryMutex_attributes = {
   .name = "telemetryMutex"
 };
 /* USER CODE BEGIN PV */
-typedef enum {
-    STATE_IDLE,
-    STATE_ARMED,
-    STATE_LAUNCH,
-    STATE_ASCENT,
-    STATE_DESCENT,
-    STATE_RECOVERY
-} FlightState;
+osThreadId_t idleTaskHandle, armedTaskHandle, launchTaskHandle, ascentTaskHandle;
+osThreadId_t descentTaskHandle, recoveryTaskHandle;
+osThreadId_t telemetryTaskHandle, sdCardSimTaskHandle;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-void StateTask(void *argument);
+void IdleTask(void *argument);
 void TelemetryTask(void *argument);
-void ErrorTask(void *argument);
-void StorageTask(void *argument);
+void SDCardSimTask(void *argument);
 
 /* USER CODE BEGIN PFP */
-void SimulateStateActions(FlightState state);
-void SaveToEEPROM(void);
-void RestoreFromEEPROM(void);
-void SimulateSDCardWrite(const char* data);
-
-
-
+void IdleTask(void *argument);
+void ArmedTask(void *argument);
+void LaunchTask(void *argument);
+void AscentTask(void *argument);
+void DescentTask(void *argument);
+void RecoveryTask(void *argument);
+void TelemetryTask(void *argument);
+void SDCardSimTask(void *argument);
+void SendTelemetry(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-#ifdef __GNUC__
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif
-
-PUTCHAR_PROTOTYPE
-{
-  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-  return ch;
-}
-
-int _write(int file, char *ptr, int len)
-{
-  int DataIdx;
-  for (DataIdx = 0; DataIdx < len; DataIdx++) {
-    __io_putchar(*ptr++);
-  }
-  return len;
+int __io_putchar(int ch) {
+    HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    return ch;
 }
 
 
-static FlightState currentState = STATE_IDLE;
-static uint32_t packetCount = 0;
-static uint32_t missionTime = 0;
-static ErrorFlags errorFlags = E_NONE;
+uint16_t packetCount = 0;
+uint16_t errorFlags = ERROR_NONE;
+char telemetryString[256];
+osMutexId_t dataMutex;
 
-void SimulateStateActions(FlightState state) {
-    switch(state) {
-        case STATE_IDLE:
-            printf("IDLE: Initializing systems and calibrating sensors\r\n");
-            break;
-        case STATE_ARMED:
-            printf("ARMED: Running pre-launch diagnostics\r\n");
-            break;
-        case STATE_LAUNCH:
-            printf("LAUNCH: Detecting launch conditions\r\n");
-            break;
-        case STATE_ASCENT:
-            printf("ASCENT: Monitoring flight data and motor burnout\r\n");
-            break;
-        case STATE_DESCENT:
-            printf("DESCENT: Deploying parachutes\r\n");
-            printf("Action: Main parachute deployment initiated\r\n");
-            break;
-        case STATE_RECOVERY:
-            printf("RECOVERY: Activating recovery systems\r\n");
-            printf("Action: GPS tracking activated\r\n");
-            printf("Action: Audio beacon activated\r\n");
-            break;
+void SDCardSimTask(void *argument) {
+    while (1) {
+        osMutexAcquire(dataMutex, osWaitForever);
+        printf("[SD CARD] Data saved: %s\n", telemetryString);
+        osMutexRelease(dataMutex);
+        osDelay(5000);
     }
 }
 
-void SaveToEEPROM(void) {
-    printf("Saving state to EEPROM: State=%d, PacketCount=%lu, MissionTime=%lu\r\n",
-    		currentState, packetCount, missionTime);
-}
-
-void SimulateSDCardWrite(const char* data) {
-    printf("SD Card Write: %s", data);
+void SendTelemetry(void) {
+    printf("[UART] Telemetry Sent: %s\n", telemetryString);
 }
 /* USER CODE END 0 */
 
@@ -207,7 +121,6 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -216,22 +129,20 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  setvbuf(stdout, NULL, _IONBF, 0);
-  printf("System Starting...\r\n");
+  dataMutex = osMutexNew(NULL);
+
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -244,40 +155,31 @@ int main(void)
   telemetryMutexHandle = osMutexNew(&telemetryMutex_attributes);
 
   /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
   /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of stateTask */
-  stateTaskHandle = osThreadNew(StateTask, NULL, &stateTask_attributes);
+  /* creation of idleTask */
+  idleTaskHandle = osThreadNew(IdleTask, NULL, &idleTask_attributes);
 
   /* creation of telemetryTask */
   telemetryTaskHandle = osThreadNew(TelemetryTask, NULL, &telemetryTask_attributes);
 
-  /* creation of errorTask */
-  errorTaskHandle = osThreadNew(ErrorTask, NULL, &errorTask_attributes);
-
-  /* creation of storageTask */
-  storageTaskHandle = osThreadNew(StorageTask, NULL, &storageTask_attributes);
+  /* creation of sdCardSimTask */
+  sdCardSimTaskHandle = osThreadNew(SDCardSimTask, NULL, &sdCardSimTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
   /* USER CODE END RTOS_EVENTS */
 
   /* Start scheduler */
@@ -287,12 +189,9 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
   /* USER CODE END 3 */
 }
 
@@ -352,11 +251,9 @@ static void MX_USART2_UART_Init(void)
 {
 
   /* USER CODE BEGIN USART2_Init 0 */
-
   /* USER CODE END USART2_Init 0 */
 
   /* USER CODE BEGIN USART2_Init 1 */
-
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
@@ -371,7 +268,6 @@ static void MX_USART2_UART_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN USART2_Init 2 */
-
   /* USER CODE END USART2_Init 2 */
 
 }
@@ -424,176 +320,142 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StateTask */
-/**
-  * @brief  Function implementing the stateTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StateTask */
-void StateTask(void *argument)
+/* USER CODE BEGIN Header_IdleTask */
+/* USER CODE END Header_IdleTask */
+void IdleTask(void *argument)
 {
   /* USER CODE BEGIN 5 */
-	  uint32_t lastButtonPress = 0;
-	  printf("State Task Started\r\n");
+    printf("System in IDLE state. Initializing systems...\n\r");
 
-  /* Infinite loop */
-  for(;;)
-  {
-	  HAL_GPIO_TogglePin(LD4_GPIO_Port, LD4_Pin);
-	  printf("a");
+    HAL_Delay(1000);
+    printf("Initialization complete. Waiting for button press to switch to ARMED state.\n\r");
 
-      if(HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
-          uint32_t currentTime = HAL_GetTick();
-          HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-          printf("c");
+    while (1) {
+        if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_SET) {
+            HAL_Delay(5000);
+            if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_SET) {
+                osThreadNew(ArmedTask, NULL, NULL);
+                osThreadExit();
+            }
+        }
+        osDelay(100);
+    }
 
-          if(currentTime - lastButtonPress > 500) {
-              osMutexAcquire(stateMutexHandle, osWaitForever);
-              printf("d");
-
-              currentState = (currentState + 1) % 6;
-
-              printf("Button pressed - New State: %d\r\n", currentState);
-
-              SimulateStateActions(currentState);
-
-              SaveToEEPROM();
-
-              osMutexRelease(stateMutexHandle);
-              lastButtonPress = currentTime;
-          }
-      }
-	  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin,SET);
-	  printf("b");
-
-      osDelay(50);
-  }
   /* USER CODE END 5 */
 }
 
 /* USER CODE BEGIN Header_TelemetryTask */
-/**
-* @brief Function implementing the telemetryTask thread.
-* @param argument: Not used
-* @retval None
-*/
 /* USER CODE END Header_TelemetryTask */
 void TelemetryTask(void *argument)
 {
   /* USER CODE BEGIN TelemetryTask */
-    char telemetryString[MAX_TELEMETRY_STRING];
+    while (!Recovered) {
+        osMutexAcquire(dataMutex, osWaitForever);
+        sprintf(telemetryString, "#%s,TimeStamp,%d,Alt,Pres,Temp,V,GNSS,Acc,Gyro,State,ErrFlags\r\n", \
+                TEAM_ID, packetCount++);
+        osMutexRelease(dataMutex);
 
-  /* Infinite loop */
-  for(;;)
-  {
-	  printf("e");
-	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-      osMutexAcquire(telemetryMutexHandle, osWaitForever);
+        SendTelemetry();
+        osDelay(100);
+    }
 
-      float altitude = 100.0f;
-      float pressure = 101.325f;
-      float temp = 25.0f;
-      float voltage = 11.8f;
-
-      int len = snprintf(telemetryString, MAX_TELEMETRY_STRING,
-              "#%s,%lu,%lu,%.2f,%.2f,%.2f,%.2f,%lu,%.6f,%.6f,%.2f,%d,%.2f,%.2f,%d,%.2f,%04X$\r\n",
-              TEAM_ID, missionTime, packetCount, altitude, pressure, temp, voltage,
-              0UL, 0.0f, 0.0f, 0.0f, 0, 0.0f, 0.0f,
-              currentState, temp, errorFlags);
-      printf("tele");
-
-      if (len > 0) {
-          HAL_UART_Transmit(&huart2, (uint8_t*)telemetryString, len, HAL_MAX_DELAY);
-
-          printf("Packet %lu sent\r\n", packetCount);
-
-          SimulateSDCardWrite(telemetryString);
-
-          packetCount++;
-          missionTime++;
-      }
-      printf("f");
-
-      osMutexRelease(telemetryMutexHandle);
-      printf("g");
-
-
-      HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-      printf("h");
-
-
-      osDelay(TELEMETRY_DELAY);
-      printf("i");
-
-  }
   /* USER CODE END TelemetryTask */
 }
 
-/* USER CODE BEGIN Header_ErrorTask */
-/**
-* @brief Function implementing the errorTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_ErrorTask */
-void ErrorTask(void *argument)
-{
-  /* USER CODE BEGIN ErrorTask */
-  /* Infinite loop */
-  for(;;)
-  {
-	  HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-	  printf("h");
-
-      float voltage = 11.8f;
-      if(voltage < 11.0f) {
-    	  printf("i");
-
-          errorFlags |= E_BATT;
-      } else {
-          errorFlags &= ~E_BATT;
-      }
-
-      osDelay(ERROR_CHECK_DELAY);
-  }
-  /* USER CODE END ErrorTask */
-}
-
-/* USER CODE BEGIN Header_StorageTask */
-/**
-* @brief Function implementing the storageTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StorageTask */
-void StorageTask(void *argument)
-{
-  /* USER CODE BEGIN StorageTask */
-  /* Infinite loop */
-  for(;;)
-  {
-	printf("1");
-    osDelay(10);
-  }
-  /* USER CODE END StorageTask */
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
+
+void ArmedTask(void *argument) {
+    printf("System in ARMED state. Performing diagnostics...\n\r");
+
+    HAL_Delay(2000);
+    printf("Diagnostics complete. Waiting for button press to LAUNCH.\n\r");
+
+    while (1) {
+        if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
+            osThreadNew(LaunchTask, NULL, NULL);
+            osThreadExit();
+        }
+        osDelay(100);
+    }
+}
+void LaunchTask(void *argument) {
+    printf("System in LAUNCH state. Detecting liftoff...\n\r");
+
+    HAL_Delay(1000);
+    printf("Liftoff detected. Switching to ASCENT state.\n\r");
+    while(1){
+		if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
+
+			osThreadNew(AscentTask, NULL, NULL);
+			osThreadExit();
+		}
+        osDelay(100);
+    }
+}
+
+void AscentTask(void *argument) {
+    printf("System in ASCENT state. Monitoring flight data...\n\r");
+
+    HAL_Delay(3000);
+    printf("Motor burnout detected. Switching to DESCENT state.\n\r");
+
+    while(1){
+		if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
+
+			osThreadNew(DescentTask, NULL, NULL);
+			osThreadExit();
+		}
+        osDelay(100);
+    }
+}
+
+void DescentTask(void *argument) {
+    printf("System in DESCENT state. Deploying parachutes...\n\r");
+
+    printf("Releasing drogue parachute.\n");
+    HAL_Delay(1000);
+    printf("Payload ejection.\n");
+    HAL_Delay(1000);
+    printf("Deploying main parachute.\n");
+    HAL_Delay(1000);
+
+    printf("Switching to RECOVERY state.\n");
+
+    while(1){
+		if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
+
+			osThreadNew(RecoveryTask, NULL, NULL);
+			osThreadExit();
+		}
+        osDelay(100);
+    }
+}
+
+void RecoveryTask(void *argument) {
+    printf("System in RECOVERY state. Blinking LED and transmitting GPS data.\n");
+
+    while (1) {
+        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+        HAL_Delay(500);
+		if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET) {
+	        Recovered = 1;
+			osThreadExit();
+		}
+
+
+    }
+}
+
+
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -608,8 +470,6 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
